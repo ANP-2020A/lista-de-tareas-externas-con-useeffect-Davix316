55708802491d68128abcf002e459d8beb68e61f9
Davix316 <david.curipoma@epn.edu.ec>
2020-08-27 17:00:11 -0500

    ListExterna

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import '../styles/App.css';
 import UserList from './UserList';
 import TodoList from './TodoList';
+import TodoListExterna from "./TodoListExterna";
 
 const App = () => {
 
     const [ viewTasks, setViewTasks ] = useState( true );
 
     return (
-        <>
+        /*<>
             <UserList />
 
             <button onClick={ () => setViewTasks( ( prevViewTasks ) => !prevViewTasks ) }>
@@ -19,8 +20,12 @@ const App = () => {
             {
                 viewTasks && <TodoList />
             }
-        </>
+
+            <TodoListExterna />
+        </>*/
+        <TodoListExterna />
     );
 };
 
 export default App;
+
